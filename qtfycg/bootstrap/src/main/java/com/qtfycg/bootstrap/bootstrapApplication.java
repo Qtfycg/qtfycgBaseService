140f7fe71e138ad963edecec7c3eb5c2f3bef104
qtfycg <1264318507@qq.com>
2026-07-04 16:59:15 +0800

    feat: :art: 后端项目初始化
    后端项目初始化

---
+++ qtfycg/bootstrap/src/main/java/com/qtfycg/bootstrap/bootstrapApplication.java
@@ -0,0 +1,14 @@
+package com.qtfycg.bootstrap;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@Slf4j
+@SpringBootApplication
+public class bootstrapApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(bootstrapApplication.class, args);
+        log.info("==========项目启动成功==========");
+    }
+}
