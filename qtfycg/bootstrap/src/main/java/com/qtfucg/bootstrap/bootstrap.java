3aae0bb4b4e630c07f1af9a57a8b247c8605a78c
qtfycg <1264318507@qq.com>
2026-09-05 14:21:07 +0800

    feat: :art: 项目启动模块初始化+github CI/CD工作流搭建
    项目启动模块初始化+github CI/CD工作流搭建

---
+++ qtfycg/bootstrap/src/main/java/com/qtfucg/bootstrap/bootstrap.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c)
+ * 2026
+ * qtfycg
+ * All rights reserved
+ */
+
+package com.qtfucg.bootstrap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class bootstrap {
+
+    public static void main(String[] args) {
+        SpringApplication.run(bootstrap.class, args);
+    }
+}
